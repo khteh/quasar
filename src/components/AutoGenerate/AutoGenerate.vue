10bae1a322b0a237664618a712f20956080457d4
Teh Kok How <funcoolgeek@gmail.com>
2022-04-18 09:17:10 +0800

    Validate specific autogenerated UI Elements using iterator index

--- src/components/AutoGenerate/AutoGenerate.vue
+++ src/components/AutoGenerate/AutoGenerate.vue
@@ -12,7 +12,7 @@
             :name="field.name"
             :fieldName="field.fieldName"
             :description="field.description"
-            data-test="toggle-setting"
+            data-test="`toggle-setting-${index}`"
           />
           <pop-up-edit-setting
             v-if="field.type === 'popUpEdit'"
@@ -20,7 +20,7 @@
             :name="field.name"
             :fieldName="field.fieldName"
             :hint="field.hint"
-            data-test="popup-edit-setting"
+            data-test="`popup-edit-setting-${index}`"
           />
           <drop-down-setting
             v-if="field.type === 'dropDown'"
@@ -28,7 +28,7 @@
             :description="field.description"
             :fieldName="field.fieldName"
             :internalOptions="internalOptions"
-            data-test="dropdown-setting"
+            data-test="`dropdown-setting-${index}`"
           />
         </div>
       </div>
